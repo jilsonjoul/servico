7e4d84c89f037b74055c6e6141bef81314f8b232
jilsonjoul <165008312+jilsonjoul@users.noreply.github.com>
2024-11-15 11:53:08 +0000

    Update script.js

--- .github/workflows/script.js
+++ .github/workflows/script.js
@@ -1,10 +1,1 @@
-// script.js
-function sayHello(name) {
-    return `Hello, ${name}!`;
-}
-
-// Exemplo de uso
-console.log(sayHello("World"));
-
-// Exportar para testes
-module.exports = sayHello;
+alert("hello");
